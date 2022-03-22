0d2147edd0b3c9f4da935e6a8c2654a8a15f29ab
mnangwe <pupuma.azabenathi@gmail.com>
2022-03-22 10:57:41 +0200

    make it better

--- index.js
+++ index.js
@@ -26,131 +26,130 @@ app.set('port', process.env.PORT || 3200)
 app.get('/', (req, res, next) =>{
     res.send({
         message: "Welcome to Azabenathi & Sergio's API",
-      //   user_routes: {
-      //     user_register: {
-      //       method: "POST",
-      //       route: "/users",
-      //       request_body: {
-      //         fullname: "String",
-      //         email: "String",
-      //         phone_number: "String",
-      //         password: "String",
-      //         cart: "object",
-      //       },
-      //       result: {
-      //         jwt: "String token",
-      //       },
-      //     },
-      //     user_login: {
-      //       method: "PATCH",
-      //       route: "/users",
-      //       request_body: {
-      //         email: "String",
-      //         password: "String",
-      //       },
-      //       result: {
-      //         jwt: "String token",
-      //       },
-      //     },
-      //     all_users: {
-      //       method: "GET",
-      //       route: "/users",
-      //       result: {
-      //         users: "Array",
-      //       },
-      //     },
-      //     single_user: {
-      //       method: "GET",
-      //       route: "/users/:id",
-      //       result: {
-      //         user: "Object",
-      //       },
-      //     },
-      //     update_user: {
-      //       method: "PUT",
-      //       request_body: {
-      //         fullname: "String",
-      //         email: "String",
-      //         phone_number: "String",
-      //         password: "String",
-      //         img: "String *optional* (Must be hosted image. I can suggest to host on Post Image)",
-      //         cart: "object",
-      //       },
-      //       route: "/users/:id",
-      //       result: {
-      //         user: "Object",
-      //       },
-      //     },
-      //     delete_user: {
-      //       method: "DELETE",
-      //       route: "/users/:id",
-      //       result: {
-      //         message: "Object",
-      //       },
-      //     },
-      //   },
-      //   product_routes: {
-      //     all_products: {
-      //       method: "GET",
-      //       route: "/products",
-      //       headers: {
-      //         authorization: "Bearer (JWT token)",
-      //       },
-      //       result: {
-      //         product: "Array",
-      //       },
-      //     },
-      //     single_product: {
-      //       method: "GET",
-      //       route: "/products/:id",
-      //       headers: {
-      //         authorization: "Bearer (JWT token)",
-      //       },
-      //       result: {
-      //         product: "Object",
-      //       },
-      //     },
-      //     create_product: {
-      //       method: "POST",
-      //       route: "/products/",
-      //       headers: {
-      //         authorization: "Bearer (JWT token)",
-      //       },
-      //       request_body: {
-      //         title: "String",
-      //         body: "String",
-      //         img: "String *optional* (Must be hosted image. I can suggest to host on Post Image)",
-      //       },
-      //       result: {
-      //         post: "Object",
-      //       },
-      //     },
-      //     update_product: {
-      //       method: "PUT",
-      //       route: "/products/:id",
-      //       headers: {
-      //         authorization: "Bearer (JWT token)",
-      //       },
-      //       request_body: {
-      //         title: "String *optional*",
-      //         body: "String *optional*",
-      //         img: "String *optional* (Must be hosted image. I can suggest to host on Post Image)",
-      //       },
-      //       result: {
-      //         post: "Object",
-      //       },
-      //     },
-      //     delete_post: {
-      //       method: "DELETE",
-      //       route: "/products/:id",
-      //       result: {
-      //         message: "Object",
-      //       },
-      //     },
-      //   },
-      // });
+        user_routes: {
+          user_register: {
+            method: "POST",
+            route: "/users",
+            request_body: {
+              fullname: "String",
+              email: "String",
+              phone_number: "String",
+              password: "String",
+              cart: "object",
+            },
+            result: {
+              jwt: "String token",
+            },
+          },
+          user_login: {
+            method: "PATCH",
+            route: "/users",
+            request_body: {
+              email: "String",
+              password: "String",
+            },
+            result: {
+              jwt: "String token",
+            },
+          },
+          all_users: {
+            method: "GET",
+            route: "/users",
+            result: {
+              users: "Array",
+            },
+          },
+          single_user: {
+            method: "GET",
+            route: "/users/:id",
+            result: {
+              user: "Object",
+            },
+          },
+          update_user: {
+            method: "PUT",
+            request_body: {
+              fullname: "String",
+              email: "String",
+              phone_number: "String",
+              password: "String",
+              img: "String *optional* (Must be hosted image. I can suggest to host on Post Image)",
+              cart: "object",
+            },
+            route: "/users/:id",
+            result: {
+              user: "Object",
+            },
+          },
+          delete_user: {
+            method: "DELETE",
+            route: "/users/:id",
+            result: {
+              message: "Object",
+            },
+          },
+        },
+        product_routes: {
+          all_products: {
+            method: "GET",
+            route: "/products",
+            headers: {
+              authorization: "Bearer (JWT token)",
+            },
+            result: {
+              product: "Array",
+            },
+          },
+          single_product: {
+            method: "GET",
+            route: "/products/:id",
+            headers: {
+              authorization: "Bearer (JWT token)",
+            },
+            result: {
+              product: "Object",
+            },
+          },
+          create_product: {
+            method: "POST",
+            route: "/products/",
+            headers: {
+              authorization: "Bearer (JWT token)",
+            },
+            request_body: {
+              title: "String",
+              body: "String",
+              img: "String *optional* (Must be hosted image. I can suggest to host on Post Image)",
+            },
+            result: {
+              post: "Object",
+            },
+          },
+          update_product: {
+            method: "PUT",
+            route: "/products/:id",
+            headers: {
+              authorization: "Bearer (JWT token)",
+            },
+            request_body: {
+              title: "String *optional*",
+              body: "String *optional*",
+              img: "String *optional* (Must be hosted image. I can suggest to host on Post Image)",
+            },
+            result: {
+              post: "Object",
+            },
+          },
+          delete_post: {
+            method: "DELETE",
+            route: "/products/:id",
+            result: {
+              message: "Object",
+            },
+          },
+        },
+      });
     });
-})
 
 
 
